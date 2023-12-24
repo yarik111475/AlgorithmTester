1d8dbe8b2b831733218e0003b558ea5fb6e420a9
yarik111475 <yarik111475@mail.ru>
2023-12-24 16:00:41 +0400

    -add fixes on win32

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
+#include <QtGlobal>
+#include <QVector>
 #include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app {argc,argv};
+    qRegisterMetaType<QVector<double>>();
     MainWindow mainWindow {};
     mainWindow.resize(800,600);
     mainWindow.show();
